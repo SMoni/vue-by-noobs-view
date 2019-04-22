e9cd1d511d97ba7d64b404863cf2f27b0b543b26
SMoni <steffen.monarth@ingenieur.io>
2019-04-22 22:26:13 +0200

    init

---
+++ with-loader/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <img src="./assets/softwerkskammer.png">
+    <HelloWorld msg="Hello World... OMG!!!ZeroOneEleven"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style lang="less">
+
+body {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+</style>
